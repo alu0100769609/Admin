8a79f3d3fb4a1a8b03241e5f5ee25507efd2851a
Adexe Sabina <alu0100769609@ull.edu.es>
2015-04-26 13:51:10 +0100

    Directorio privado de usuario y algo de estilos

--- javascripts/funciones.js
+++ javascripts/funciones.js
@@ -1,3 +1,17 @@
 function FuncNoDisp() {
     alert("Lo sentimos, esta opción no está disponible");
 }
+
+//Se encarga de mostrar el botón cuando se hace scroll
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 500) {
+    $('.arrowtop').fadeIn();
+  } else {
+    $('.arrowtop').fadeOut();
+  }
+});
+//Se encarga de subir60 al top al hacer clic en el botón
+function arriba(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+}
